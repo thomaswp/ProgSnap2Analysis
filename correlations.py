6a5bf8ac733bbb814ed5e494ffd3d47bfcce5fe1
Thomas Price <twprice@ncsu.edu>
2019-08-23 14:59:30 -0400

    Added correlation p values

--- correlations.py
+++ correlations.py
@@ -1,11 +1,22 @@
 import pandas as pd
 import sys
 import os
-# from scipy import stats
+from scipy.stats import pearsonr
+from scipy.stats import spearmanr
+
+
+def calculate_correlation_pvalues(df, fn=pearsonr):
+    df = df.dropna()._get_numeric_data()
+    df_cols = pd.DataFrame(columns=df.columns)
+    p_values = df_cols.transpose().join(df_cols, how='outer')
+    for r in df.columns:
+        for c in df.columns:
+            p_values[r][c] = fn(df[r], df[c])[1]
+    return p_values
 
 
 if __name__ == "__main__":
-    read_path = "./data/CloudCoder"
+    read_path = "./data"
     out_dir = "./out"
 
     if len(sys.argv) > 1:
@@ -27,13 +38,17 @@ if __name__ == "__main__":
         grades_table = grades_table.merge(metric_table, on=["SubjectID"])
     count_dict["Merged"] = grades_table.shape[0]
 
-    pearson = grades_table.drop("SubjectID", axis=1).corr(method="pearson")
+    metrics = grades_table.drop("SubjectID", axis=1)
+
+    pearson = metrics.corr(method="pearson")
     print(pearson)
     pearson.to_csv(os.path.join(out_dir, "corr_pearson.csv"))
+    calculate_correlation_pvalues(metrics).to_csv(os.path.join(out_dir, "corr_pearson_p.csv"))
 
     spearman = grades_table.drop("SubjectID", axis=1).corr(method="spearman")
     print(spearman)
     spearman.to_csv(os.path.join(out_dir, "corr_spearman.csv"))
+    calculate_correlation_pvalues(metrics, spearmanr).to_csv(os.path.join(out_dir, "corr_spearman_p.csv"))
 
     print(count_dict)
     with open(os.path.join(out_dir, "counts.txt"), 'w') as file:
@@ -45,3 +60,4 @@ if __name__ == "__main__":
     # r_squared = stats.linregress(x, y)[2] ** 2
     # print(r_squared)
     # r_squared.to_csv(os.path.join(out_dir, "r_squared.csv"))
+
