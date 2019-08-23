fbfdcb73553998de3a3c3e9d84505a4355711d7f
Thomas Price <twprice@ncsu.edu>
2019-08-23 14:41:32 -0400

    Added output for Ns

--- correlations.py
+++ correlations.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import sys
 import os
-from scipy import stats
+# from scipy import stats
 
 
 if __name__ == "__main__":
-    read_path = "./data"
+    read_path = "./data/CloudCoder"
     out_dir = "./out"
 
     if len(sys.argv) > 1:
@@ -14,6 +14,8 @@ if __name__ == "__main__":
         out_dir = sys.argv[2]
 
     grades_table = pd.read_csv(os.path.join(read_path, "LinkTables/Subject.csv"))
+    count_dict = {}
+    count_dict["grades"] = grades_table.shape[0]
 
     for metric in ["EQ", "Watwin", "RED"]:
         path = os.path.join(out_dir, metric + ".csv")
@@ -21,19 +23,25 @@ if __name__ == "__main__":
             continue
         print("Found: " + path)
         metric_table = pd.read_csv(path)
+        count_dict[metric] = metric_table.shape[0]
         grades_table = grades_table.merge(metric_table, on=["SubjectID"])
+    count_dict["Merged"] = grades_table.shape[0]
 
     pearson = grades_table.drop("SubjectID", axis=1).corr(method="pearson")
     print(pearson)
     pearson.to_csv(os.path.join(out_dir, "corr_pearson.csv"))
 
-    pearson = grades_table.drop("SubjectID", axis=1).corr(method="spearman")
+    spearman = grades_table.drop("SubjectID", axis=1).corr(method="spearman")
     print(spearman)
-    pearson.to_csv(os.path.join(out_dir, "corr_spearman.csv"))
+    spearman.to_csv(os.path.join(out_dir, "corr_spearman.csv"))
 
-    data_table = grades_table.drop("SubjectID", axis=1)
-    x = data_table.iloc[:, 0]
-    y = data_table.iloc[:, 1]
-    r_squared = stats.linregress(x, y)[2] ** 2
-    print(r_squared)
-    r_squared.to_csv(os.path.join(out_dir, "r_squared.csv"))
+    print(count_dict)
+    with open(os.path.join(out_dir, "counts.txt"), 'w') as file:
+        file.write(str(count_dict))
+
+    # data_table = grades_table.drop("SubjectID", axis=1)
+    # x = data_table.iloc[:, 0]
+    # y = data_table.iloc[:, 1]
+    # r_squared = stats.linregress(x, y)[2] ** 2
+    # print(r_squared)
+    # r_squared.to_csv(os.path.join(out_dir, "r_squared.csv"))
