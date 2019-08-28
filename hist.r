94266ff167f275cb3ca8985f9138ae7e7f98fc51
thomaswp <thomaswprice@msn.com>
2019-08-28 18:04:20 -0400

    Added R script

--- hist.r
+++ hist.r
@@ -1,11 +1,35 @@
 library(ggplot2)
 
+
+data <- data.frame()
+for (dir in c("CC", "CWO", "BlockPy", "ITAP", "PCRS")) {
+  for (file in c("EQ", "RED", "WatWin")) {
+    path <- paste0("out/", dir, "/", file, ".csv")
+    if (!file.exists(path)) {
+      print(paste("Missing:", path))
+      next
+    }
+    metric <- read.csv(path)
+    print(head(metric))
+    names(metric)[2] <- "Value"
+    metric$SubjectID <- as.character(metric$SubjectID)
+    metric$Metric <- if (file == "WatWin") "Watwin" else file
+    metric$Dataset <- dir
+    if (nrow(data) == 0)
+      data <- metric
+    else
+      data <- rbind(data, metric)
+  }
+}
+
+ggplot(data, aes(Value)) + geom_density(fill="gray") + 
+  scale_x_continuous(breaks = c(0, 0.5, 1)) + 
+  scale_y_continuous(name="Density", limits=c(0,6)) +
+  theme_bw() + #ggtitle("Metric Distributions") +
+  facet_grid(Metric ~ Dataset)
+
 n <- 150
 test <- data.frame(Value=runif(n*5*3,0,1), 
                    Metric=rep(c("EQ", "RED", "Watwin"), n), 
                    Dataset=c(rep("CC", n), rep("CWO", n), rep("BlockPy", n), rep("PCRS", n), rep("ITAP", n)))
 
-ggplot(test, aes(Value)) + geom_histogram(bins=10) + 
-  scale_x_continuous(breaks = c(0, 0.5, 1)) + scale_y_continuous(name="Count") +
-  theme_bw() + #ggtitle("Metric Distributions") +
-  facet_grid(Metric ~ Dataset)
